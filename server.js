30f0dc216bdb7118dbf8f5cda65dd68ec27c095b
AnkitCBIQ Profile01 <inscribe04@gmail.com>
2026-05-18 18:12:32 +0530

    Fix CORS to allow all Vercel deployments

--- server.js
+++ server.js
@@ -100,7 +100,13 @@ app.use('/login',    authLimiter);
 app.use('/register', authLimiter);
 
 app.use(cors({
-    origin: 'https://myproject01-react-mapu2b3m6-ankit-cbiq-s-projects.vercel.app',
+    origin: function(origin, callback) {
+        if (!origin || origin.endsWith('.vercel.app')) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 
